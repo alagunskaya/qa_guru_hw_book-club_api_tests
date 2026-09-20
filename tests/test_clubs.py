7c63cf34d05c247441aeabdd9b760f2820f1e9d1
ALagunskaya <nastya.emotion@gmail.com>
2026-09-21 03:27:08 +0500

    Update tests

--- tests/test_clubs.py
+++ tests/test_clubs.py
@@ -6,11 +6,6 @@ from schemas.schema import CLUBS_LIST_SCHEMA
 BASE_URL = "https://book-club.qa.guru/api/v1/clubs/"
 
 
-def test_get_clubs_status_200():
-    response = requests.get(BASE_URL)
-    assert response.status_code == 200
-
-
 def test_get_clubs_has_results():
     # в ответе есть список клубов
     response = requests.get(BASE_URL)
@@ -29,24 +24,31 @@ def test_get_clubs_matches_schema():
     validate(instance=data, schema=CLUBS_LIST_SCHEMA)
 
 
+def test_get_clubs_real_content():
+    response = requests.get(BASE_URL)
+    first_club = response.json()["results"][0]
+
+    assert len(first_club["bookTitle"].strip()) >= 3
+    assert len(first_club["bookAuthors"].strip()) >= 3
+    assert isinstance(first_club["publicationYear"], int)
+
+
 def test_get_clubs_page_size():
     # page_size ограничивает количество элементов
     response = requests.get(BASE_URL, params={"page": 1, "page_size": 2})
     data = response.json()
 
     assert response.status_code == 200
-    assert len(data["results"]) <= 2
+    assert len(data["results"]) == 2
 
 
 def test_search_clubs():
-    # поиск первого клуба
     first_club = requests.get(BASE_URL).json()["results"][0]
     search_item = first_club["bookTitle"]
-    print(f"\n{search_item}")
 
     response = requests.get(BASE_URL, params={"search": search_item})
     data = response.json()
-    print(response.url)
 
     assert response.status_code == 200
     assert data["count"] >= 1
+    assert search_item in data["results"][0]["bookTitle"]
